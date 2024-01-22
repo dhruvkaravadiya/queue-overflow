85aa41d3dda45a668ec909dbedc4e0e62ed4a199
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-01-22 19:41:20 +0530

    added auth signin and signup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "../globals.css";
+const Layout = ({ children }: { children: React.ReactNode }) => {
+      return (<main className="flex-center">{children}</main>);
+}
+
+export default Layout;
---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+const AskQuestion = () => {
+      return (<><h1>Ask Question</h1></>);
+}
+
+export default AskQuestion;
