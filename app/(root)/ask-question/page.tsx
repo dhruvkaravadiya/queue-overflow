ffea974988f97089afb1dc1b16bd35238fb133f1
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-02-21 21:21:48 +0530

    made user sign up dynamic with clerk id

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import QuestionForm from "@/app/components/Forms/QuestionForm";
 import { getUserById } from "@/database/actions/user.action";
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
 const AskQuestion = async () => {
-    const clerkId = "123456789";
-    if (!clerkId) {
+    const { userId } = auth();
+    if (!userId) {
         redirect("/sign-in");
     }
-    const mongoUser = await getUserById(clerkId);
+    const mongoUser = await getUserById(userId);
     return (
         <>
             <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
