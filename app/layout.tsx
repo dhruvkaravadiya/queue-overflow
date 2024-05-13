f22b79dd9c972d038a84c0ca44783e6a95a7fec6
Dhruv Karavadiya <karavadiadhruv22@gmail.com>
2024-05-13 09:19:53 +0530

    updated metadata description

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { ThemeProvider } from "./context/ThemeProvider";
 
 export const metadata: Metadata = {
     title: "Queue Overflow",
-    description: "Thriving Community of Geeks",
+    description: "Thriving Community of Developers and Geeks",
     icons: {
         icon: "/assets/icons/site-logo.svg",
     },
